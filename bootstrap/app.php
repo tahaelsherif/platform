de35b59cb9cc90e9d41ec572af4a1c5673135528
tahaelsherif <taha95elsherif@gmail.com>
2021-03-03 18:39:30 +0200

    fix apache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,4 +14,10 @@ require_once __DIR__.'/../src/Init.php';
 
 $app = require __DIR__.'/lumen.php';
 
+
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers', 'DNT,X-ApiKey,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
+header('Access-Control-Expose-Headers', 'DNT,X-ApiKey,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
+
 return $app;
